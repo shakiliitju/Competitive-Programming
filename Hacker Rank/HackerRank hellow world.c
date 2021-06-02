533edd9e8ce9c77ee1a25a535e5d289a7d52b679
Md. Shakil Hossain <shakiljusc15@gmail.com>
2021-06-02 08:23:27 +0600

    Hacker Rank

---
+++ Hacker Rank/HackerRank hellow world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char n[100];
+    int x,tc,ts;
+    for (tc=0; tc<ts; tc++)
+    {
+        scanf("%d",&x);
+    }
+    gets(n);
+    printf("Hello,World!\n");
+    printf("%s",n);
+
+    return 0;
+}
